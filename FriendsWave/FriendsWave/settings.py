eef4a9b4c7285ceb02f7e9f25269309e66264957
mk-josias <chefeuloreoo@gmail.com>
2021-11-28 02:02:34 +0100

    database register

--- FriendsWave/FriendsWave/settings.py
+++ FriendsWave/FriendsWave/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social.apps.SocialConfig'
+    'social.apps.StoreSocial',
     'rest_framework', 
 ]
 
@@ -132,3 +132,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
